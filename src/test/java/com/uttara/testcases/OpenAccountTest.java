4566f7e8ef0f6f846f8cfc2678492e223b21c796
Chandru <chandruks83@gmail.com>
2018-11-18 16:16:08 +0530

    New tests

---
+++ src/test/java/com/uttara/testcases/OpenAccountTest.java
@@ -0,0 +1,12 @@
+package com.uttara.testcases;
+
+import org.testng.annotations.Test;
+
+import com.uttara.base.TestBase;
+
+public class OpenAccountTest extends TestBase{
+	@Test
+	public void openAccountTest() {
+		
+	}
+}
